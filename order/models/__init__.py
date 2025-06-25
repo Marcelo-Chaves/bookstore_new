480a207901d009c3bb047336800ca17e0abadb93
marcelo chaves <marceloluiz.icg@gmail.com>
2025-06-24 22:06:23 -0400

    adicionando os códigos dos tests

---
+++ order/models/__init__.py
@@ -0,0 +1,6 @@
+# logo após não pode deixar de registrar no admin.py
+# from django.contrib import admin
+# from .models import Order
+# admin .site.register(Order)
+
+from .order import Order
---
+++ product/models/__init__.py
@@ -0,0 +1,10 @@
+# não esquecer de registrar no admin.py
+#from django.contrib import admin
+#from .models import Category, Product
+#admin.site.register(Category)
+#admin.site.register(Product)
+#realizar o as migrações (makemigrations product e migrate)
+
+
+from .category import Category
+from .product import Product
