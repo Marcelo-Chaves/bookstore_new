79e1402d82c598aa4c7e57394cd19fdb856e454e
marcelo chaves <marceloluiz.icg@gmail.com>
2025-07-06 12:31:04 -0400

    adicionando a paginação e criando novos modelos para testes

--- book/settings.py
+++ book/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+"DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",  # ← ativa a visualização bonita
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
