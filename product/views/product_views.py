162462c3ba700bd3367c1fbc7c8901e437109380
marcelo chaves <marceloluiz.icg@gmail.com>
2025-09-11 08:31:19 -0400

    atualizando o código para acesso

--- product/views/product_views.py
+++ product/views/product_views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
-from product.models.product import Product
+from rest_framework import viewsets, permissions
+from product.models import Product
 from product.serializers.product_serializers import ProductSerializer
 from product.permissions.custom import IsManagerOrReadOnly
 
